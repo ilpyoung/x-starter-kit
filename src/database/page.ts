66cda0ec43fb6139fa562b4f429c3c8b4b0262dd
Cloud User <centos@ip-172-26-2-94.ap-northeast-2.compute.internal>
2023-06-14 05:54:16 +0000

    fix page

--- src/database/page.ts
+++ src/database/page.ts
@@ -1,5 +1,6 @@
 import { IsEnum, IsNumber, IsOptional } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
 
 export enum SortType {
     DESC = "DESC",
@@ -10,11 +11,13 @@ export class Page {
     @ApiProperty({ description: "페이지" })
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     page?: number;
 
     @ApiProperty({ description: "가져올 리스트의 limit 수" })
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     limit?: number;
 }
 
